4248acc734943d78bce4006354794b73d178dd26
Foparty <alfonsomartingarcia@gmail.com>
2023-05-01 12:07:25 +0200

    buttonempty arrow

--- src/components/ButtonEmpty/ButtonEmpty.jsx
+++ src/components/ButtonEmpty/ButtonEmpty.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from './ButtonEmpty.module.css';
+import { HiArrowLongRight } from 'react-icons/hi2';
 
 const ButtonEmpty = ({ text }) => {
   return (
     <button className={styles.button}>
       <span>{text}</span>
-      <div className={styles.arrowcontainer}>
-        <div className={styles.arrowline}></div>
-        <div className={styles.arrow}></div>
-      </div>
+      {/* <div className={styles.arrowcontainer}> */}
+      <HiArrowLongRight />
+      {/* <div className={styles.arrowline}></div>
+        <div className={styles.arrow}></div> */}
+      {/* </div> */}
     </button>
   );
 };
